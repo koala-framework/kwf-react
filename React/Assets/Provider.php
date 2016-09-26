6279e779ad11755ee499d18ccdaf940a3ef688e6
Alexander Kaufmann <ak@vivid-planet.com>
2016-09-26 10:12:41 +0200

    support importing folder only when index.jsx file exists

--- React/Assets/Provider.php
+++ React/Assets/Provider.php
@@ -8,11 +8,17 @@ class React_Assets_Provider extends Kwf_Assets_Provider_Abstract
             if (file_exists(KWF_PATH.'/commonjs/'.$dependencyName.'.jsx')) {
                 $ret = new React_Assets_Dependency_File_Jsx($this->_providerList, 'kwf/commonjs/'.$dependencyName.'.jsx');
                 return $ret;
+            } else if (file_exists(KWF_PATH.'/commonjs/'.$dependencyName.'/index.jsx')) {
+                $ret = new React_Assets_Dependency_File_Jsx($this->_providerList, 'kwf/commonjs/'.$dependencyName.'/index.jsx');
+                return $ret;
             }
         } else if (substr($dependencyName, 0, 4) == 'web/') {
             $dependencyName = substr($dependencyName, 4);
             if (file_exists('./'.$dependencyName.'.jsx')) {
                 $ret = new React_Assets_Dependency_File_Jsx($this->_providerList, 'web/'.$dependencyName.'.jsx');
+                return $ret;
+            } else if (file_exists('./'.$dependencyName.'/index.jsx')) {
+                $ret = new React_Assets_Dependency_File_Jsx($this->_providerList, 'web/'.$dependencyName.'/index.jsx');
                 return $ret;
             }
         }
